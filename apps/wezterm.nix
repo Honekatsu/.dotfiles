f3919f918aebda96d412c433fa27280ddaa8cc08
ReyoKatsu <38076269+Honekatsu@users.noreply.github.com>
2025-08-15 12:24:44 +0900

    refactor(wezterm): add wezterm inputs

--- apps/wezterm.nix
+++ apps/wezterm.nix
@@ -1,11 +1,14 @@
 {
-  inputs,
   pkgs,
    ...
 }:
+
+let
+  weztermFlake = builtins.getFlake "github:wez/wezterm?dir=nix";
+in 
 {
   programs.wezterm = {
-    package = inputs.wezterm.packages.${pkgs.system}.default;
+    package = weztermFlake.packages.${pkgs.system}.default;
     enable = true;
     enableZshIntegration = true;
     extraConfig = builtins.readFile ./wezterm.lua;
